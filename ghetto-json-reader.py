b133961e9867aac533fdb4ef5dc8167fee4618ab
LastWulfStanding <LastWulfStanding@protonmail.com>
2019-01-04 05:32:10 -0500

    Added documentation to quotes_spider.py and introduced ghetto-json-reader.py

---
+++ ghetto-json-reader.py
@@ -0,0 +1,13 @@
+
+# Code from Mils Werner
+# Found on the website: https://stackoverflow.com/questions/2835559/parsing-values-from-a-json-file
+
+import json
+from pprint import pprint
+
+filename = input("Name of file:\n")
+
+with open(filename + '.json') as f:
+    data = json.load(f)
+    
+pprint(data)
--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -5,24 +5,30 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
+    # Name of the quotes, names of the spiders have to be unique to one another
     name = "quotes"
+    
+    # The urls that the spider starts with
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
     ]
 
     def parse(self, response):
+        # for loop gets every <div class="quote" ...> for information extraction
         for quote in response.css('div.quote'):
             yield {
-                'text': quote.css('span.text::text').extract_first(),
-                'author': quote.css('small.author::text').extract_first(),
-                'tags': quote.css('div.tags a.tag::text').extract(),
+                'text': quote.css('span.text::text').extract_first(),       # Extracts the text inside the <span class="text"> tags
+                'author': quote.css('small.author::text').extract_first(),  # Extracts the text inside the <small class="author"> tags 
+                'tags': quote.css('div.tags a.tag::text').extract(),        # Extracts the text inside the <div class="tags"><a class="tag"> tags
             }
         
-        #page = response.url.split("/")[-2]
-        #filename = 'quotes-%s.html' % page
-        #with open(filename, 'wb') as f:
-            #f.write(response.body)
+        next_page = response.css('li.next a::attr(href)').extract_first()   # Extracts the href inside the <li class="next"><a> tags
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse())
+            
+            #next_page = response.urljoin(next_page)
+            #yield scrapy.Request(next_page, callback=self.parse)
+        
 
 
 #import scrapy
